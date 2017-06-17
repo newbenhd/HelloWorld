9fcce5c205040c02e901b2bee22db9b6415c0cd1
newbenhd <newben.hd@gmail.com>
2017-06-16 23:26:36 -0700

    Added destroy, pause, resume

--- app/src/main/java/com/example/newben/helloworld/MainActivity.java
+++ app/src/main/java/com/example/newben/helloworld/MainActivity.java
@@ -7,11 +7,25 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
