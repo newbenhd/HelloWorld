d1f25fa6060e03e1d5fc8d0cb985b071c32a34ce
newbenhd <newben.hd@gmail.com>
2017-06-16 23:04:46 -0700

    Initial commit

---
+++ app/src/main/java/com/example/newben/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.newben.helloworld;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
